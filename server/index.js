d36f3afe39e3508a3909ee71f82a502ff000dfcb
Anzori-PR <saxelashvilianzori@gmail.com>
2025-03-06 12:47:36 +0400

    Enhance CORS configuration and update Vercel routing for improved API handling

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const corsConfig = {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
+app.options("", cors(corsConfig));
 app.use(cors(corsConfig));
 app.use(express.json());
 
