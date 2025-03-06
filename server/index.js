7dba2b4875db0cf81e4abdf580bdb05a50e7a311
Anzori-PR <saxelashvilianzori@gmail.com>
2025-03-06 12:24:50 +0400

    Deploy

--- server/index.js
+++ server/index.js
@@ -17,6 +17,8 @@ require('dotenv').config();
 
 
 mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB:', err));
 
 const storage = multer.diskStorage({
     destination: './uploads/company-logos/',
